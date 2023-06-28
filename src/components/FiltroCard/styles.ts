6674d07563cb6f8f91be9fb4408c5e5abc95ff58
Fernando Pinho <fernandoalbertopinho20@gmail.com>
2023-06-28 11:23:05 +0100

    analisar

---
+++ src/components/FiltroCard/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Card = styled.div`
+  padding: 8px;
+  border: 1px solid #a1a1a1;
+  background-color: #fcfcfc;
+  color: #5e5e5e;
+`
+
+export const Contador = styled.div`
+  font-weight: bold;
+  font-size: 24px;
+  display: block;
+`
+
+export const Label = styled.div`
+  font-size: 14px;
+`
